73826e9dfb3246c4823c7d73af370da8abaeceef
20岁码农想当导演 <45226880+942418430@users.noreply.github.com>
2018-11-27 22:26:29 +0800

    Create 左旋字符串.c
    //实现一个函数，可以左旋字符串中的k个字符。 
    //ABCD左旋一个字符得到BCDA 
    //ABCD左旋两个字符得到CDAB

---
+++ 左旋字符串.c
@@ -0,0 +1,22 @@
+
+//实现一个函数，可以左旋字符串中的k个字符。 
+//ABCD左旋一个字符得到BCDA 
+//ABCD左旋两个字符得到CDAB 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	char arr[] ="abcdefg";
+	int n = 0;
+	int k = 0;
+	printf("输入左旋的第k个字符k=：\n");
+	scanf("%d",&k);
+	n = strlen(arr);
+
+	printf(
+	system("pause");
+	return 0;
+}
