74915095ff7341d30d1f351c17100f6825458d4c
942418430 <942418430@qq.com>
2018-12-09 20:56:35 +0800

    提交静态通讯录代码

---
+++ 12_9_1/12_9_1/contact.h
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define max_name 20
+#define max_sex 4
+#define max_tln 11
+#define max_adds 30
+#define max_count 1000
+
+enum P
+{
+	ext,
+	add,
+	gai,
+	cha,
+	dayin,
+	shan,
+	paixu
+};
+
+//定义联系人结构体
+typedef struct pp
+{
+	char name[max_name];
+	int age;
+	char sex[max_sex];
+	char tln[max_tln];
+	char adds[max_adds];
+}pp;
+
+//定义通讯录结构体
+typedef struct con
+{
+	pp com[max_count];
+	int count;
+}con;
+
+void chushihua(con*);
+void Add(con*);
+void Dayin(con*);
+void Cha(con*);
+void Gai(con*);
+void Shan(con*);
+void Paixu(con*);
---
+++ 12_9_1/12_9_1/test.c
@@ -0,0 +1,69 @@
+#include"contact.h"
+
+void meun()
+{
+	printf("**********************************************\n");
+	printf("******   1.add   ******     2.gai       ******\n");
+	printf("**********************************************\n");
+	printf("******   3.cha   ******     4.dayin     ******\n");
+	printf("**********************************************\n");
+	printf("******   5.shan  ******     6.paixu     ******\n");
+	printf("**********************************************\n");
+	printf("****************  0.ext  *********************\n");
+	printf("**********************************************\n");
+}
+
+void test()
+{
+	int input = 0;
+	con cn;
+	//初始化
+	chushihua(&cn);
+	do
+	{
+		meun();
+		printf("请选择：\n");
+		scanf("%d",&input);
+		switch(input)
+		{
+		case ext:
+			printf("退出！\n");
+			break;
+		case add:
+			printf("增加联系人！\n");
+			//增加联系人
+			Add(&cn);
+			break;
+		case gai:
+			//修改联系人信息
+			Gai(&cn);
+			break;
+		case cha:
+			//查找联系人信息
+			Cha(&cn);
+			break;
+		case dayin:
+			//打印通讯录
+			Dayin(&cn);
+			break;
+		case shan:
+			//删除联系人
+			Shan(&cn);
+			break;
+		case paixu:
+			//进行排序并打印
+			Paixu(&cn);
+			break;
+		default:
+			printf("无此选项！\n");
+		}
+	}while(input);
+}
+
+int main()
+{
+	//静态
+	test();
+	system("pause");
+	return 0;
+}
